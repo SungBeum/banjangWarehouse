1971a1b23e9c16f2f31551b32bd00e9a29c3b774
SungBeum <an4126@gmail.com>
2024-01-14 01:18:27 +0900

    feat: home page implement

--- banjangwarehouse/tailwind.config.js
+++ banjangwarehouse/tailwind.config.js
@@ -1,8 +1,37 @@
+const plugin = require("tailwindcss/plugin");
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ["./src/**/*.{html,js}"],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{html,js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        "custom-orange": {
+          DEFAULT: "#F75A2F",
+          10: "#F75A2F1A",
+        },
+        "custom-black": "#0000004D",
+        "custom-gray": {
+          DEFAULT: "#C1C1C1",
+          light: "#D9D9D9",
+        },
+      },
+    },
+  },
+  plugins: [
+    plugin(function ({ addUtilities }) {
+      addUtilities({
+        ".routingButton": {
+          "@apply text-[25px] leading-[30.26px] bg-white font-normal text-black  px-[11px] py-[21px] rounded-[20px]":
+            {},
+        },
+        ".eventPopup": {
+          "@apply text-[13px] leading-[18px] font-normal text-white p-1 rounded-[10px] tracking-[0.16px]":
+            {},
+        },
+        ".commonText": {
+          "@apply text-[18px] leading-[21.78px] font-normal text-black": {},
+        },
+      });
+    }),
+  ],
+};

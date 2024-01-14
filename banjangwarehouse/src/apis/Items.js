80581c56a08e39d9dafca799708431cbdfd8ca30
SungBeum <an4126@gmail.com>
2024-01-15 01:26:00 +0900

    chore: add not found page

--- banjangwarehouse/src/apis/Items.js
+++ banjangwarehouse/src/apis/Items.js
@@ -3,7 +3,8 @@ export const getItems = async () => {
     const response = await fetch("http://localhost:3001/items");
     return response.json();
   } catch (error) {
-    console.error("에러가 발생했습니다.");
+    alert("목록을 불러오는데 에러가 발생했습니다 !");
+    console.error(error);
     throw error;
   }
 };
--- banjangwarehouse/src/constants/path.js
+++ banjangwarehouse/src/constants/path.js
@@ -1,2 +1,3 @@
+export const HOME = "/";
 export const ORDER = "/order";
 export const COMPLETE = "/complete";
